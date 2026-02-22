b682254eb070d97a4439ade5973905b6569b8523
pegn0ly <owafe001@gmail.com>
2026-02-22 14:50:04 +0300

    fixed sidebar layout

--- src/components/body/editor/dialogGenerator/globals.tsx
+++ src/components/body/editor/dialogGenerator/globals.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mantine/core";
+import { Button, Stack } from "@mantine/core";
 import { useCurrentDialogId } from "./store"
 import { invoke } from "@tauri-apps/api/core";
 
@@ -6,9 +6,11 @@ function DialogGeneratorGlobals() {
     const currentDialogId = useCurrentDialogId();
     
     return (
-    <Button size="xs" onClick={() => invoke("generate_dialog", {dialogId: currentDialogId})} disabled={currentDialogId == undefined} radius={0}>
-        Generate current dialog
-    </Button>
+        <Stack>
+            <Button size="xs" onClick={() => invoke("generate_dialog", {dialogId: currentDialogId})} disabled={currentDialogId == undefined} radius={0}>
+                Generate current dialog
+            </Button>
+        </Stack>
     )
 }
 
